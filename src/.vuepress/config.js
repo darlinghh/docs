f9c6031e8c354470e24483aafd1118c958022c2b
admin <wangwulliuqibajiu@gmail.com>
2024-01-21 21:14:12 +0800

    init

---
+++ src/.vuepress/config.js
@@ -0,0 +1,20 @@
+import { defineUserConfig } from "vuepress";
+import { defaultTheme } from "vuepress";
+export default defineUserConfig({
+	lang: "zh-CN",
+	title: "你好， VuePress ！",
+	description: "这是我的第一个 VuePress 站点",
+	theme: defaultTheme({
+		// 默认主题配置
+		navbar: [
+			{
+				text: "首页",
+				link: "/",
+			},
+			{
+				text: "home",
+				link: "/home/",
+			},
+		],
+	}),
+});
---
+++ src/.vuepress/dist/assets/index.html-mUrCvOJ2.js
@@ -0,0 +1,1 @@
+const e=JSON.parse('{"key":"v-14b0a7d7","path":"/home/","title":"","lang":"zh-CN","frontmatter":{},"headers":[],"git":{},"filePathRelative":"home/README.md"}');export{e as data};
